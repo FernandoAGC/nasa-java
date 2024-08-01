75255fea2cc097573a2684e5815a0e9bbbd4bf9a
FernandoAGC <fernando.a.galicia.c@gmail.com>
2024-08-01 11:52:42 -0600

    update: consumo de api nasa manaejando exepciones
    Se actualizó el codigo para obtener los datos externos de la api rest de la nasa con manejo de exepciones.

--- src/main/java/com/nasa/prueba/aspirante/infraestructura/clientrest/NasaClientRest.java
+++ src/main/java/com/nasa/prueba/aspirante/infraestructura/clientrest/NasaClientRest.java
@@ -1,5 +1,7 @@
 package com.nasa.prueba.aspirante.infraestructura.clientrest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,13 +13,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class NasaClientRest {
 
+    private static final Logger logger = LoggerFactory.getLogger(NasaClientRest.class);
     private final RestTemplate restTemplate;
     private static final String NASA_API_BASE_URL = "https://images-api.nasa.gov/search?q=";
 
     // consumir api rest
     public NasaDataDto getNasaData(String query) {
         String url = NASA_API_BASE_URL + query;
-        return restTemplate.getForObject(url, NasaDataDto.class);
+        try {
+            return restTemplate.getForObject(url, NasaDataDto.class);
+        } catch (Exception e) {
+            logger.error("Error while fetching data from NASA API: {}", e.getMessage());
+            return null;
+        }
     }
     
 }
